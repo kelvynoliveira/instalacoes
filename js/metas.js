4054500633c74368e341cb3c2189bc6a7efa86d8
kelvynr <kelvynsilveirarodrigo@outlook.com>
2025-05-21 18:05:35 -0300

    FIREBASEOK

--- js/metas.js
+++ js/metas.js
@@ -1,58 +1,58 @@
 export const metas = {
   UNA: {
-    "CATALÃO": { switch: 2 },
-    "KARAIBA": { switch: 2 },
-    "BETIM": { switch: 2, nobreak: 1 },
-    "BOMDESPACHO": { switch: 2 },
-    "SETELAGOAS": { switch: 2 },
-    "UBERLANDIA": { nobreak: 2 },
-    "POUSOALEGRE": { switch: 2 }
+    "UNA|CATALÃO": { switch: 2 },
+    "UNA|KARAIBA": { switch: 2 },
+    "UNA|BETIM": { switch: 2, nobreak: 1 },
+    "UNA|BOMDESPACHO": { switch: 2 },
+    "UNA|SETELAGOAS": { switch: 2 },
+    "UNA|UBERLANDIA": { nobreak: 2 },
+    "UNA|POUSOALEGRE": { switch: 2 }
   },
   AGES: {
-    "SENHORDOBONFIM": { switch: 2, nobreak: 1 },
-    "PARIPIRANGA": { switch: 1, nobreak: 1 },
-    "JACOBINA": { switch: 1, nobreak: 1 },
-    "IRECE": { nobreak: 1 }
+    "AGES|SENHORDOBONFIM": { switch: 2, nobreak: 1 },
+    "AGES|PARIPIRANGA": { switch: 1, nobreak: 1 },
+    "AGES|JACOBINA": { switch: 1, nobreak: 1 },
+    "AGES|IRECE": { nobreak: 1 }
   },
   FASEH: {
-    "VESPASIANO": { switch: 2 }
+    "FASEH|VESPASIANO": { switch: 2 }
   },
   UNIBH: {
-    "ESTORIL": { switch: 10, nobreak: 10 }
+    "UNIBH|ESTORIL": { switch: 10, nobreak: 10 }
   },
   UNIFG: {
-    "GUANAMBI": { switch: 2, nobreak: 1 }
+    "UNIFG|GUANAMBI": { switch: 2, nobreak: 1 }
   },
   UNP: {
-    "SALGADOFILHO": { switch: 10 },
-    "MOSSORO": { switch: 2 }
+    "UNP|SALGADOFILHO": { switch: 10 },
+    "UNP|MOSSORO": { switch: 2 }
   },
   UNIFACS: {
-    "TANCREDONEVES": { switch: 3, nobreak: 5 }
+    "UNIFACS|TANCREDONEVES": { switch: 3, nobreak: 5 }
   },
   FG: {
-    "PIEDADE": { switch: 2, nobreak: 1 }
+    "FG|PIEDADE": { switch: 2, nobreak: 1 }
   },
   IBMR: {
-    "BARRA": { switch: 2, nobreak: 1 }
+    "IBMR|BARRA": { switch: 2, nobreak: 1 }
   },
   UAM: {
-    "MOOCA": { switch: 5, nobreak: 5 },
-    "PAULISTA": { switch: 3, nobreak: 1 },
-    "VILAOLIMPIA": { switch: 3 }
+    "UAM|MOOCA": { switch: 5, nobreak: 5 },
+    "UAM|PAULISTA": { switch: 3, nobreak: 1 },
+    "UAM|VILAOLIMPIA": { switch: 3 }
   },
   UNR: {
-    "ZONASUL": { switch: 10, nobreak: 1 },
-    "CANOAS": { switch: 4, nobreak: 1 }
+    "UNR|ZONASUL": { switch: 10, nobreak: 1 },
+    "UNR|CANOAS": { switch: 4, nobreak: 1 }
   },
   UNISUL: {
-    "TUBARAO": { switch: 5, nobreak: 2 }
+    "UNISUL|TUBARAO": { switch: 5, nobreak: 2 }
   },
   UNISOCIESC: {
-    "BLUMENAU": { switch: 5, nobreak: 1 },
-    "ANITAGARIBALDI": { switch: 2 }
+    "UNISOCIESC|BLUMENAU": { switch: 5, nobreak: 1 },
+    "UNISOCIESC|ANITAGARIBALDI": { switch: 2 }
   },
   HSM: {
-    "LEARNINGVILLAGE": { switch: 4, nobreak: 1 }
+    "HSM|LEARNINGVILLAGE": { switch: 4, nobreak: 1 }
   }
 };
