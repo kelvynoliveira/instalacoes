77f4cbb4d9969c73ad1920434f594b184fc19393
kelvynr <kelvynsilveirarodrigo@outlook.com>
2025-05-21 09:54:35 -0300

    metajs

---
+++ js/metas.js
@@ -0,0 +1,58 @@
+export const metas = {
+  UNA: {
+    "CATALÃO": { switch: 2 },
+    "KARAIBA": { switch: 2 },
+    "BETIM": { switch: 2, nobreak: 1 },
+    "BOMDESPACHO": { switch: 2 },
+    "SETELAGOAS": { switch: 2 },
+    "UBERLANDIA": { nobreak: 2 },
+    "POUSOALEGRE": { switch: 2 }
+  },
+  AGES: {
+    "SENHORDOBONFIM": { switch: 2, nobreak: 1 },
+    "PARIPIRANGA": { switch: 1, nobreak: 1 },
+    "JACOBINA": { switch: 1, nobreak: 1 },
+    "IRECE": { nobreak: 1 }
+  },
+  FASEH: {
+    "VESPASIANO": { switch: 2 }
+  },
+  UNIBH: {
+    "ESTORIL": { switch: 10, nobreak: 10 }
+  },
+  UNIFG: {
+    "GUANAMBI": { switch: 2, nobreak: 1 }
+  },
+  UNP: {
+    "SALGADOFILHO": { switch: 10 },
+    "MOSSORO": { switch: 2 }
+  },
+  UNIFACS: {
+    "TANCREDONEVES": { switch: 3, nobreak: 5 }
+  },
+  FG: {
+    "PIEDADE": { switch: 2, nobreak: 1 }
+  },
+  IBMR: {
+    "BARRA": { switch: 2, nobreak: 1 }
+  },
+  UAM: {
+    "MOOCA": { switch: 5, nobreak: 5 },
+    "PAULISTA": { switch: 3, nobreak: 1 },
+    "VILAOLIMPIA": { switch: 3 }
+  },
+  UNR: {
+    "ZONASUL": { switch: 10, nobreak: 1 },
+    "CANOAS": { switch: 4, nobreak: 1 }
+  },
+  UNISUL: {
+    "TUBARAO": { switch: 5, nobreak: 2 }
+  },
+  UNISOCIESC: {
+    "BLUMENAU": { switch: 5, nobreak: 1 },
+    "ANITAGARIBALDI": { switch: 2 }
+  },
+  HSM: {
+    "LEARNINGVILLAGE": { switch: 4, nobreak: 1 }
+  }
+};
